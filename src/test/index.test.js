3780037caa5873da717a0794b5098d422e53ede5
刘纯涛 <1786215856@qq.com>
2024-05-16 19:26:46 +0800

    init

---
+++ src/test/index.test.js
@@ -0,0 +1,10 @@
+import { describe, it } from 'vitest';
+import Konva from 'konva';
+
+describe('', () => {
+  it('', () => {
+    new Konva.Stage(); // error
+    new Konva.Layer(); // error
+    new Konva.Rect(); // pass
+  });
+});
